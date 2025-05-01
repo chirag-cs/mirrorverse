d0dc26845adabeb04a605ff1882b556f12708970
Chirag Sharma <chirag.sharma@microtekdirect.com>
2025-05-01 15:37:18 +0530

    deployment-changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,16 @@
-// client/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: path.resolve(__dirname, '../server/client-dist'),
+    outDir: resolve(__dirname, '../server/client-dist'),
     emptyOutDir: true,
   },
 });
