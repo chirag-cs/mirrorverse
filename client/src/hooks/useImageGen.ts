e9c2c8c611e4e1815d506ce78904e0905ecb2dd2
Chirag Sharma <chirag.sharma@microtekdirect.com>
2025-05-01 20:32:58 +0530

    testing-102

--- client/src/hooks/useImageGen.ts
+++ client/src/hooks/useImageGen.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/";
+const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
 
 export const generateImage = async (prompt: string): Promise<string> => {
